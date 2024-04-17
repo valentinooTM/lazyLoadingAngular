c8bcf48dd78e9b054deadf04fbde5997563d04e2
valentinooTM <92047590+valentinooTM@users.noreply.github.com>
2024-04-17 10:18:08 +0200

    Add files via upload
    coms

---
+++ src/app/com1/com1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-com1',
+  templateUrl: './com1.component.html',
+  styleUrl: './com1.component.css'
+})
+export class Com1Component {
+
+}
---
+++ src/app/com2/com2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-com2',
+  templateUrl: './com2.component.html',
+  styleUrl: './com2.component.css'
+})
+export class Com2Component {
+
+}
